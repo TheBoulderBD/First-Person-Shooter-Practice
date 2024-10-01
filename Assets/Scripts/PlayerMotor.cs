aa050dd26968d02584bb57c7a60d7f3cd0f089f1
Isah Aminu <155932354+TheBoulderBD@users.noreply.github.com>
2024-10-01 15:00:22 -0400

    Player Camera
    The player is able to look around using the mouse.

--- Assets/Scripts/PlayerMotor.cs
+++ Assets/Scripts/PlayerMotor.cs
@@ -10,6 +10,7 @@ public class PlayerMotor : MonoBehaviour
     public float speed = 5f;
     public float gravity = -9.8f;
     public float jumpHeight = 3f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -27,14 +28,17 @@ public class PlayerMotor : MonoBehaviour
         moveDirection.x = input.x;
         moveDirection.z = input.y;
         controller.Move(transform.TransformDirection(moveDirection)* speed * Time.deltaTime);
-        playerVelocity.y = gravity * Time.deltaTime;   
+        playerVelocity.y += gravity * Time.deltaTime;
 
-        if(isGrounded && playerVelocity.y < 0)
+        if (isGrounded && playerVelocity.y < 0)
         {
             playerVelocity.y = -2f;
         }
 
-        controller.Move(playerVelocity* Time.deltaTime);   
+        controller.Move(playerVelocity * Time.deltaTime);
+        Debug.Log(playerVelocity.y);
+
+
     }
 
     public void Jump()
