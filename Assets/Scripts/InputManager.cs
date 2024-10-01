a987bfffe4267e48de54284d1dd66fbb8923e25d
Isah Aminu <isaminu2004@gmail.com>
2024-10-01 13:22:06 -0400

    Fixed Input
    Began working on jump function

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -8,20 +8,23 @@ public class InputManager : MonoBehaviour
     private PlayerInput playerInput;
     private PlayerInput.OnFootActions onFoot;
 
+    private PlayerMotor motor;
     // Start is called before the first frame update
     void Awake()
     {
         playerInput = new PlayerInput();
         onFoot = playerInput.OnFoot;
+        motor = GetComponent<PlayerMotor>();
+        onFoot.Jump.performed += ctx => motor.Jump();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        motor.ProcessMove(onFoot.Movement.ReadValue<Vector2>());
     }
 
-    private void onEnable()
+    private void OnEnable()
     {
         onFoot.Enable();
     }
